ad575494c5d6c6c2fce753645d9dae2be0abc734
Nick Tikhonov <nt34@st-andrews.ac.uk>
2016-07-09 12:13:07 -0700

    Initial commit

---
+++ spec/codefitness-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodefitnessView from '../lib/codefitness-view';
+
+describe('CodefitnessView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
